720bf303ce367f12a6633def85b657e40bc1b34d
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-03-25 19:09:49 +0300

    logo again

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,7 @@ export default function Sidebar(){
     return(
         <aside className="fixed bg-[#3498db] text-gray-200 z-50 h-full shadow-lg shadow-gray-900/20 transition duration-300 ease-in-out w-[20rem]">
             <div className="flex relative items-center py-5 px-3.5">
-                <Image alt="RCHS System" src='/RCHS_logo.png' className='w-12 mx-3.5 min-h-fit' width={35} height={35}></Image>
+                <Image alt="RCHS System" src='/logo.png' className='w-12 mx-3.5 min-h-fit' width={35} height={35}></Image>
                 <h3 className="pl-2 font-bold text-2xl">RCHS System</h3>
             </div>
         </aside>
