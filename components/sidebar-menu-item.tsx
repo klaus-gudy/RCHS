1023cb5df75e8d8f7ad5e2db2fbbf2f2034fc2c1
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-03-26 15:55:04 +0300

    sidebar item selection

--- components/sidebar-menu-item.tsx
+++ components/sidebar-menu-item.tsx
@@ -1,25 +1,34 @@
 "use client";
 import { SideNavItem } from "@/types/types";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { BsChevronRight } from "react-icons/bs";
 
 export const SideBarMenuItem = ({ item }: { item: SideNavItem }) => {
   const linkStyle =
-    "flex items-center min-h-[40px] h-full text-gray-200  py-2 px-4 hover:text-blue-600 rounded-md transition duration-200";
+    "flex items-center min-h-[40px] h-full text-gray-200  py-2 px-4 hover:text-gray-800 rounded-md transition duration-200";
+  const activelinkStyle =
+    "rounded-md text-white light:text-black light:bg-[#efefef] bg-blue-400";
   const ddlinkStyle = linkStyle;
   const navMenuDropdownItem =
     "text-white py-2 px-4 hover:text-blue-600 transition duration-200";
 
+  const pathName = usePathname();
   const [subMenuOpen, setSubMenuOpen] = useState(false);
-  const toggleSubMenu=()=>{
+  const toggleSubMenu = () => {
     setSubMenuOpen(!subMenuOpen);
-  }
+  };
   return (
     <>
       {item.submenu ? (
         <div className="rounded-md min-w-[18px]">
-          <a className={ddlinkStyle} onClick={toggleSubMenu}>
+          <a
+            className={`${ddlinkStyle} ${
+              pathName.includes(item.path) ? activelinkStyle : ""
+            }`}
+            onClick={toggleSubMenu}
+          >
             {item.icon}
             <span className="ml-3 text-base leading-6 font-semibold">
               {item.title}
@@ -34,7 +43,7 @@ export const SideBarMenuItem = ({ item }: { item: SideNavItem }) => {
                     <Link
                       key={index}
                       href={subItem.path}
-                      className={navMenuDropdownItem}
+                      className={`${navMenuDropdownItem} ${subItem.path===pathName?'text-gray-800':''}`}
                     >
                       <span>{subItem.title}</span>
                     </Link>
@@ -45,7 +54,12 @@ export const SideBarMenuItem = ({ item }: { item: SideNavItem }) => {
           )}
         </div>
       ) : (
-        <Link href={item.path} className={linkStyle}>
+        <Link
+          href={item.path}
+          className={`${linkStyle} ${
+            item.path === pathName ? activelinkStyle : ""
+          }`}
+        >
           {item.icon}
           <span className="ml-3 leading-6 font-semibold">{item.title}</span>
         </Link>
