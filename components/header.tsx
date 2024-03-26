e589d650e7d2bfc82eca9d7de3735ee96e1fc55d
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-03-26 10:23:37 +0300

    header changes

--- components/header.tsx
+++ components/header.tsx
@@ -2,12 +2,15 @@ import { BsList } from "react-icons/bs";
 
 export default function Header(){
     return(
-        <div className="fixed bg-[#3498db] w-full z-0 px-4 shadow-sm shadow-slate-500/40 pl-[20rem]">
-            <div className="flex items-center justify between h-16">
+        <header className="fixed bg-[#3498db] w-full z-0 px-4 shadow-sm shadow-slate-500/40 pl-[20rem]">
+            <div className="flex items-center justify-between h-16">
                 <button className="bg-[#6ca2ff] text-[#ffffff] hover:bg-blue-500 ml-3 rounded-md h-[30px] w-[30px] shadow-md shadow-black/10 transition duration-300 ease-in-out flex items-center justify-center">
                     <BsList></BsList>
                 </button>
+                <div className="h-10 w-10 rounded-full bg-[#6ca2ff] flex items-center justify-center text-center">
+                    <span className="font-semibold text-sm">GM</span>
+                </div>
             </div>
-        </div>
+        </header>
     );
 }
