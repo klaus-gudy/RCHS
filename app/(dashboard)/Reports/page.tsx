48cfc1dd49ef4a27838ea7512791cad1889822ba
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-05-06 17:42:33 +0300

    Replace patient-statsss component with demo component
    - Removed: AboutStatisticsss component
    - Added: Demo component from @/app/components/graphs/demo

--- app/(dashboard)/Reports/page.tsx
+++ app/(dashboard)/Reports/page.tsx
@@ -1,14 +1,13 @@
+import Demo from "@/app/components/graphs/demo";
 import AboutStatistics from "@/app/components/patient-stats";
 import AboutStatisticss from "@/app/components/patient-statss";
-import AboutStatisticsss from "@/app/components/patient-statsss";
-import Image from "next/image";
 
 export default function Reports() {
   return (
     <>
       <AboutStatistics />
       <AboutStatisticss />
-      <AboutStatisticsss />
+      <Demo />
     </>
   );
 }
